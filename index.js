df2b15ffd6a400b12d78c63fc6fa3944a02c73b9
Hakobjan2007 <hakobjan2007@gmail.com>
2023-08-27 18:57:04 +0400

    FindPair

---
+++ index.js
@@ -0,0 +1,20 @@
+var nums = [-1,8,9,4,6,5];
+var k = 10;
+
+function FindPair(nums, k)
+{
+	var map = new Map();
+	for(var i = 0; i < nums.length; i++)
+	{
+		if(map.has(k - nums[i]))
+		{
+			console.log(`Pairs => ${nums[i]}, ${nums[map.get((k - nums[i]))]}`);
+			return;
+		}
+		map.set(nums[i], i);
+	}
+
+	console.log("Cannot find pairs");
+}
+
+FindPair(nums, k);
